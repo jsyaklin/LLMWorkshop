9c0a1c114c5be63871e5edc3bddb078c8f348fa2
Ansh Tulsyan <atulsyan850@gmail.com>
2025-09-25 17:35:30 -0500

    updated link

--- chatbot.py
+++ chatbot.py
@@ -2,11 +2,32 @@ import os
 
 #   Import libraries here
 #
-#   Refer to https://docs.llamaindex.ai/en/stable/examples/llm/gemini/ for help
+from llama_index.core.llms import ChatMessage
+from llama_index.llms.google_genai import GoogleGenAI
+
+
+
+#   Refer to https://developers.llamaindex.ai/python/examples/llm/google_genai/ for help
 #
 
 GOOGLE_API_KEY = ""  # add your GOOGLE API key here
-os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+os.environ["GOOGLE_API_KEY"] = "AIzaSyBHoN7nIQswCc1Ie25dmbNyfSf8VKAhNHc"
+
+llm = GoogleGenAI(
+    model="models/gemini-2.5-flash",
+    # api_key="some key",  # uses GOOGLE_API_KEY env var by default
+)
+
+messages = [
+    ChatMessage(role="user", content="Hello friend!"),
+    ChatMessage(role="assistant", content="Yarr what is shakin' matey?"),
+    ChatMessage(
+        role="user", content="Help me decide what to have for dinner."
+    ),
+]
+
+resp = llm.chat(messages)
+print(resp)
 
 
 # Write code to create a chatbot
